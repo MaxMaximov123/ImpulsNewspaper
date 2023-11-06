11af436048f96adaa0ca3bd189c3d5ef35b92f5b
Smirnov Maxim <maxss.kzn@gmail.com>
2023-11-06 14:59:33 +0300

    recreate design

--- site/src/components/Posts.vue
+++ site/src/components/Posts.vue
@@ -1,98 +1,55 @@
 <template>
     <div class="q-pa-md">
-      <div class="news-container justify-center q-gutter-sm" @scroll="handleScroll" >
+      <div class="news-container justify-center q-gutter-sm" @scroll="handleScroll">
         <div
           v-for="(post, index) in postList"
           :key="post.key"
           :data-id="index - 1"
-          class="news-item q-pa-sm flex-center relative-position"
+          class="news-item q-pa-sm"
         >
-          <transition name="q-transition--scale">
-            <q-card>
-              <div v-if="post.text[0]">
+          <q-card style="background-color: rgba(240, 235, 232, 0.797);">
+            <div v-if="post.text[0] && post.images?.[0]" class="news-info items-start">
+              <div class="col" style="margin-top: auto; margin-bottom: auto;">
                 <q-card-section>
-                  <div v-html="post.text[0] + (
-                    post.text[1] && post.allText ? post.text[1] : ''
-                  )"></div>
-                  <div v-if="post.text[1] && !post.allText">
+                  <div class="news-title" v-html="
+                    post.text.slice(0, 10).join(' ') + (
+                    post.text.length > 10 ? '...' : '')
+                  "></div>
+                </q-card-section>
+              </div>
+
+              <div class="col">
+                <q-card-section>
+                  <q-img
+                      class="rounded-borders large-image"
+                      :src=post.images[0]
+                      alt="large-image"
+                  />
+                </q-card-section>
+              </div>
+            </div>
+            <div v-else-if="post.text[0]" class="news-info items-start">
+              <div style="margin-top: auto; margin-bottom: auto; margin-left: auto; margin-right: auto;" class="col-6">
+                <q-card-section>
+                  <div class="news-title" v-html="post.text"></div>
+                  <div v-if="post.text && !post.allText">
                     <a :onClick="() => showAllText(post)" style="color: rgb(0, 119, 255);">Показать больше</a>
                   </div>
                 </q-card-section>
               </div>
-
-              <!-- <div class="q-pa-md">
-                <q-carousel
-                  swipeable
-                  animated
-                  v-model="post.slide"
-                  thumbnails
-                  infinite
-                >
-                  <q-carousel-slide v-for="(image, index) in post.images" :name="index+1" :img-src="image">{{index + 1}}</q-carousel-slide>
-                </q-carousel>
-              </div> -->
-
-              <div v-if="post.images.length > 0">
+            </div>
+            <div v-else class="items-start">
+              <div style="margin-left: auto; margin-right: auto;" class="once-image">
                 <q-card-section>
-                  <div v-if="post.images.length === 1">
-                    <q-img
-                        class="rounded-borders"
-                        :src=post.images[0]
-                        alt="Large Image"
-                    />
-                  </div>
-                  <div v-if="post.images.length === 2" class="q-col-gutter-md row items-start">
-                    <div v-for="index in 2" class="col-6">
-                      <q-img
-                          :src=post.images[index-1]
-                          class="rounded-borders"
-                          alt="Large Image"
-                          :ratio="4/3"
-                      />
-                    </div>
-                  </div>
-                  <div v-if="post.images.length >= 3" class="q-col-gutter-md">
-                    <div>
-                      <q-img
-                          class="rounded-borders"
-                          :src=post.images[0]
-                          alt="Large Image"
-                      />
-                    </div>
-                    <div class="q-col-gutter-md row items-start">
-                      <div v-for="index in Math.min(3, post.images.length-1)" class="col">
-                        <q-img
-                            :src=post.images[index]
-                            class="rounded-borders"
-                            alt="Large Image"
-                            :ratio="Math.max(1, 10/(post.images.length-1)/3)"
-                        />
-                      </div>
-                      <div v-if="post.images.length-5 === 0" class="col">
-                        <q-img
-                            :src=post.images[4]
-                            class="rounded-borders"
-                            alt="Large Image"
-                            :ratio="Math.max(1, 10/(post.images.length-1)/3)"
-                        />
-                      </div>
-                      <div v-if="post.images.length-1 > 4" class="col">
-                        <q-img 
-                            :src=post.images[4]
-                            class="rounded-borders"
-                            alt="Large Image"
-                            :ratio="Math.max(1, 10/(post.images.length-1)/3)">
-                          <div class="absolute-full text-subtitle2 flex flex-center" style="backdrop-filter: blur(2px);">
-                            +{{ post.images.length - 4 }}
-                          </div>
-                        </q-img>
-                      </div>
-                    </div>
-                  </div>
+                  <q-img
+                      class="rounded-borders large-image"
+                      :src=post.images[0]
+                      alt="Large Image"
+                  />
                 </q-card-section>
               </div>
-            </q-card>
-          </transition>
+            </div>
+          </q-card>
         </div>
       </div>
     </div>
@@ -169,8 +126,10 @@
             let obj = requestResults[index];
             obj.images = Array.from(new Set(obj.images)) || [];
             obj.text = obj.text.replace(/\n/g, "<br>");
-            obj.text = obj.text.replace(/Show more/g, `Показать ещё`);
-            obj.text = obj.text.split(`Показать ещё`);
+            // obj.text = obj.text.replace(/Show more/g, `Показать ещё`);
+            obj.text = obj.text.replace(/#.*/g, ``);
+            obj.text = obj.text.split(` `);
+            console.log(obj.text);
             obj.allText = false;
             obj.slide = ref(1);
             return obj;
@@ -194,43 +153,60 @@
   
   <style scoped>
   .news-item {
-    font-size: 1em;
-    width: 30%; /* Используем проценты вместо px */
+    max-width: 80%;
+    max-height: 500px;
     margin-bottom: 20px;
-    background-color: #f4f4f4;
-    border-radius: 10px;
+    background-color: #ebe1c583;
+    border-radius: 5px;
     padding: 20px;
     margin-left: auto;
-    margin-right: auto;
-    
+    margin-right: auto; 
   }
 
-  @media (max-width: 900px) {
-  
-    .news-item {
-      width: 60%;
-    }
+  .news-title {
+    color: rgb(255, 113, 95);
+    font-family: Comic Sans Ms;
+    font-size: 30px;
   }
 
-  @media (max-width: 700px) {
-  
-    .news-item {
-      width: 95%;
-    }
+  .news-info {
+    display: flex;
+  }
+
+  .large-image {
+    max-height: 400px;
+  }
+
+  .once-image {
+    max-width: 50%;
   }
 
   .news-container {
+    margin-left: auto;
+    margin-right: auto;
     margin-top: 80px;
-    justify-content: center;
   }
 
-  .small-images {
-      display: flex;
-      justify-content: space-between;
-  }
+  @media (max-width: 700px) {
 
-  .small-image {
-      max-width: 50%; /* Ширина меньших фотографий */
-      height: auto;
+    .news-info {
+      display: block;
+    }
+
+    .large-image {
+      max-height: 300px;
+    }
+  
+    .news-item {
+      max-width: 95%;
+    }
+
+    .news-title {
+      font-size: 14px;
+    }
+
+    .once-image {
+      max-width: 100%;
+    }
   }
   </style>
