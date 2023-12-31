b9547a58d6019dfbb4acb7a0486fe0ed81b7ef7f
maxss.kzn <maxss.kzn@gmail.com>
2023-12-31 11:38:50 +0300

    upd

--- server/src/server.js
+++ server/src/server.js
@@ -81,7 +81,7 @@ app.get('*', (req, res) => {
 });
 
 // Запустите сервер на указанном порту
-const port = 443;
+const port = 80;
 const server = app.listen(port, () => {
   const address = server.address();
   console.log(`Сервер запущен на порту ${address.port}`);
