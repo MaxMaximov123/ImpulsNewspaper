30253b9db64d0ee4ffff4fd577d5fdf74ead939d
MaxMaximov123 <99262907+MaxMaximov123@users.noreply.github.com>
2024-02-26 08:27:45 +0300

    Update scanner.js

--- vk-scanner/src/scanner.js
+++ vk-scanner/src/scanner.js
@@ -184,7 +184,7 @@ export default class Scanner {
               await db('images').insert(
                 post.images.map(src => {
                   return {
-                    postKey: `${config.sourceKey}-${post.key}`,
+                    postKey: `${post.key}`,
                     src: src
                   }
                 })
